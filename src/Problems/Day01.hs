c7b8cbe192e5fa97be930598070f53b6fe679979
Stephen Nicholas Swatman <stephen@v25.nl>
2023-12-03 09:48:38 +0100

    Solution to day 1

--- src/Problems/Day01.hs
+++ src/Problems/Day01.hs
@@ -1,9 +1,35 @@
 module Problems.Day01 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Control.Arrow ((&&&))
+
+import Common.Solution (Day)
+
+concatInts :: (Integer, Integer) -> Integer
+concatInts (a, b) = 10 * a + b
+
+readDigits :: Bool -> String -> [Integer]
+readDigits _ [] = []
+readDigits b    ('1':xs)                 = (1:readDigits b xs)
+readDigits b    ('2':xs)                 = (2:readDigits b xs)
+readDigits b    ('3':xs)                 = (3:readDigits b xs)
+readDigits b    ('4':xs)                 = (4:readDigits b xs)
+readDigits b    ('5':xs)                 = (5:readDigits b xs)
+readDigits b    ('6':xs)                 = (6:readDigits b xs)
+readDigits b    ('7':xs)                 = (7:readDigits b xs)
+readDigits b    ('8':xs)                 = (8:readDigits b xs)
+readDigits b    ('9':xs)                 = (9:readDigits b xs)
+readDigits True ('o':'n':'e':xs)         = (1:readDigits True ('n':'e':xs))
+readDigits True ('t':'w':'o':xs)         = (2:readDigits True ('w':'o':xs))
+readDigits True ('t':'h':'r':'e':'e':xs) = (3:readDigits True ('h':'r':'e':'e':xs))
+readDigits True ('f':'o':'u':'r':xs)     = (4:readDigits True ('o':'u':'r':xs))
+readDigits True ('f':'i':'v':'e':xs)     = (5:readDigits True ('i':'v':'e':xs))
+readDigits True ('s':'i':'x':xs)         = (6:readDigits True ('i':'x':xs))
+readDigits True ('s':'e':'v':'e':'n':xs) = (7:readDigits True ('e':'v':'e':'n':xs))
+readDigits True ('e':'i':'g':'h':'t':xs) = (8:readDigits True ('i':'g':'h':'t':xs))
+readDigits True ('n':'i':'n':'e':xs)     = (9:readDigits True ('i':'n':'e':xs))
+readDigits b (_:xs) = readDigits b xs
 
 solution :: Day
-solution = (
-        notImplemented,
-        notImplemented
-    )
+solution = (f False, f True)
+    where
+        f b = show . sum . map (concatInts . (head &&& last) . readDigits b) . lines
--- src/Problems/Day02.hs
+++ src/Problems/Day02.hs
@@ -1,9 +1,44 @@
 module Problems.Day02 (solution) where
 
+import Text.Parsec (sepEndBy1, sepBy, newline, string, char, space, (<|>))
+
 import Common.Solution (Day, notImplemented)
+import Common.Parse (AocInput, aocParse, integer)
+
+data Round = Round
+    { red :: Integer
+    , green :: Integer
+    , blue :: Integer
+    }
+    deriving Show
+
+instance Monoid Round where
+    mempty = Round { red=0, green=0, blue=0 }
+    mconcat
+        (Round { red=r1, green=g1, blue=b1 })
+        (Round { red=r2, green=g2, blue=b2 })
+        = Round { red=r1 + r2, green=g1 + g2, blue = b1 + b2}
+
+data Game = Game
+    { id :: Integer
+    , rounds :: [Round]
+    }
+    deriving Show
+
+input :: AocInput () GameState
+input = do
+    _ <- string "Game "
+    id <- integer
+    _ <- string ": "
+    rounds <- sepEndBy1 round (string "; ")
+    return Game { id=id, rounds=rounds }
+    where
+        round = do
+            subRounds <- sepEndBy1 subRound (string ", ")
+            return fold subRounds
 
 solution :: Day
 solution = (
-        notImplemented,
+        show . aocParse input (),
         notImplemented
     )
