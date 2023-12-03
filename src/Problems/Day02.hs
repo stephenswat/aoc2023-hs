9dbf5ef383565052e9e6c94bce547a401413e4b2
Stephen Nicholas Swatman <stephen@v25.nl>
2023-12-03 10:39:07 +0100

    Add solutions to day 2

--- src/Problems/Day02.hs
+++ src/Problems/Day02.hs
@@ -1,8 +1,8 @@
 module Problems.Day02 (solution) where
 
-import Text.Parsec (sepEndBy1, sepBy, newline, string, char, space, (<|>))
+import Text.Parsec (sepEndBy1, sepBy1, newline, string, space, choice)
 
-import Common.Solution (Day, notImplemented)
+import Common.Solution (Day)
 import Common.Parse (AocInput, aocParse, integer)
 
 data Round = Round
@@ -12,33 +12,58 @@ data Round = Round
     }
     deriving Show
 
-instance Monoid Round where
-    mempty = Round { red=0, green=0, blue=0 }
-    mconcat
+instance Semigroup Round where
+    (<>)
         (Round { red=r1, green=g1, blue=b1 })
         (Round { red=r2, green=g2, blue=b2 })
         = Round { red=r1 + r2, green=g1 + g2, blue = b1 + b2}
 
+instance Monoid Round where
+    mempty = Round { red=0, green=0, blue=0 }
+
 data Game = Game
-    { id :: Integer
+    { num :: Integer
     , rounds :: [Round]
     }
     deriving Show
 
-input :: AocInput () GameState
-input = do
-    _ <- string "Game "
-    id <- integer
-    _ <- string ": "
-    rounds <- sepEndBy1 round (string "; ")
-    return Game { id=id, rounds=rounds }
+possibleA :: Game -> Bool
+possibleA g = and [red r <= 12 && green r <= 13 && blue r <= 14 | r <- rounds g]
+
+scoreB :: Game -> Integer
+scoreB g = (red m) * (green m) * (blue m)
     where
-        round = do
-            subRounds <- sepEndBy1 subRound (string ", ")
-            return fold subRounds
+        roundMax r1 r2 = Round
+            { red = max (red r1) (red r2)
+            , green = max (green r1) (green r2)
+            , blue = max (blue r1) (blue r2)
+            }
+        m = foldr1 roundMax (rounds g)
+
+input :: AocInput () [Game]
+input = sepEndBy1 game newline
+    where
+        subRound = do
+            n <- integer
+            _ <- space
+            s <- choice [string "red", string "green", string "blue"]
+            return Round
+                { red=if s == "red" then n else 0
+                , green=if s == "green" then n else 0
+                , blue=if s == "blue" then n else 0
+                }
+        gameRound = do
+            subRounds <- sepBy1 subRound (string ", ")
+            return (mconcat subRounds)
+        game = do
+            _ <- string "Game "
+            n <- integer
+            _ <- string ": "
+            gameRounds <- sepBy1 gameRound (string "; ")
+            return Game { num=n, rounds=gameRounds }
 
 solution :: Day
 solution = (
-        show . aocParse input (),
-        notImplemented
+        show . sum . map num . filter possibleA . aocParse input (),
+        show . sum . map scoreB . aocParse input ()
     )
