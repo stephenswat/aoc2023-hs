52ab1e3d510aef613b04dc2f13cacc9327ef77b0
Stephen Nicholas Swatman <stephen@v25.nl>
2023-12-06 11:32:52 +0100

    Add solution to day 6

--- src/Problems/Day06.hs
+++ src/Problems/Day06.hs
@@ -1,9 +1,31 @@
 module Problems.Day06 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Text.Parsec (sepEndBy1, string, char, many1)
+import Data.List (singleton)
+import Control.Arrow ((&&&))
+
+import Common.Solution (Day)
+import Common.Parse (AocInput, aocParse, integer)
+
+input :: AocInput () [(Integer, Integer)]
+input = do
+    _ <- string "Time:"
+    _ <- many1 (char ' ')
+    i <- sepEndBy1 integer (many1 (char ' '))
+    _ <- string "\nDistance:"
+    _ <- many1 (char ' ')
+    j <- sepEndBy1 integer (many1 (char ' '))
+    return (zip i j)
+
+solve :: (Integer, Integer) -> Integer
+solve (t, r) = toInteger . length . filter (> r) $ [c * (t - c) | c <- [0..t]]
+
+transformB :: [(Integer, Integer)] -> (Integer, Integer)
+transformB = (f fst) &&& (f snd)
+    where f g = read . concat . map show . g . unzip
 
 solution :: Day
 solution = (
-        notImplemented,
-        notImplemented
+        show . product . map solve . aocParse input (),
+        show . product . map solve . singleton . transformB . aocParse input ()
     )
