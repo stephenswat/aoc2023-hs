b06121c1cd0c2a96baa68a9614551c280f268742
Stephen Nicholas Swatman <stephen@v25.nl>
2023-12-11 12:23:43 +0100

    Add solutions to day 11

--- src/Problems/Day11.hs
+++ src/Problems/Day11.hs
@@ -1,9 +1,28 @@
 module Problems.Day11 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.Map (keys, mapKeys, filter)
+
+import Common.Solution (Day)
+import Common.Geometry (Grid2D, readGrid2DWith, manhattan)
+
+dilate :: Integer -> Grid2D Bool -> Grid2D Bool
+dilate v g = mapKeys keyMap g
+    where
+        (valX, valY) = unzip . keys . Data.Map.filter id $ g
+        emptyX = [x | x <- [0..(maximum valX)], not (elem x valX)]
+        emptyY = [y | y <- [0..(maximum valY)], not (elem y valY)]
+        keyMap (x, y) =
+            ( x + (v - 1) * (toInteger . length . Prelude.filter (<= x) $ emptyX)
+            , y + (v - 1) * (toInteger . length . Prelude.filter (<= y) $ emptyY)
+            )
+
+solve :: Grid2D Bool -> Integer
+solve g = sum [manhattan p1 p2 | p1 <- c, p2 <- c, p1 < p2]
+    where
+        c = keys . Data.Map.filter id $ g
 
 solution :: Day
 solution = (
-        notImplemented,
-        notImplemented
+        show . solve . dilate 2       . readGrid2DWith (== '#'),
+        show . solve . dilate 1000000 . readGrid2DWith (== '#')
     )
