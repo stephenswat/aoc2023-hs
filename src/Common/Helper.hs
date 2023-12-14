5fa644082a30e2e8959451b2247b162c53d80917
Stephen Nicholas Swatman <stephen@v25.nl>
2023-12-14 15:50:21 +0100

    Add a helper for cyclic function execution

--- src/Common/Helper.hs
+++ src/Common/Helper.hs
@@ -1,4 +1,6 @@
-module Common.Helper (runWhile, scanWhile, pairwise) where
+module Common.Helper (runWhile, scanWhile, pairwise, iterateCyclic) where
+
+import Data.Map (insert, lookup, empty)
 
 runWhile :: (a -> (a, Bool)) -> a -> a
 runWhile f i = let (r, c) = f i in if c then runWhile f r else r
@@ -9,3 +11,16 @@ scanWhile f i = let (r, c) = f i in if c then i:(scanWhile f r) else [r]
 pairwise :: [a] -> [(a, a)]
 pairwise (x:y:zs) = (x, y):(pairwise (y:zs))
 pairwise _ = []
+
+iterateCyclic :: Ord a => (a -> a) -> Integer -> a -> a
+iterateCyclic f n = go 0 empty
+    where
+        go i m v
+            | i == n = v
+            | (Just k) <- l =
+                if (n - i) >= (i - k)
+                then go (i + ((n - i) `div` (i - k)) * (i - k)) m v
+                else go (i + 1) m (f v)
+            | otherwise = go (i + 1) (insert v i m) (f v)
+            where
+                l = Data.Map.lookup v m
