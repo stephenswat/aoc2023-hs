ad2a4fe62b16e63c1f6c4ac1ad939684418c2de7
Stephen Nicholas Swatman <stephen@v25.nl>
2023-12-15 13:34:08 +0100

    Refactor main code

--- src/Main.hs
+++ src/Main.hs
@@ -58,18 +58,13 @@ solutions =
     ]
 
 main :: IO ()
-main = do {
-    args <- getArgs;
-    let
-        dayn = read (args !! 0) :: Int
-        sols = solutions !! (dayn - 1)
-        fnam = args !! 1
-    in do {
-        putStrLn ("Solutions for day " ++ show dayn);
-        file <- readFile fnam;
-        let (solA, solB) = sols in do {
-            putStrLn . ("Problem A: " ++) . solA $ file;
-            putStrLn . ("Problem B: " ++) . solB $ file;
-        }
-    }
-}
+main = do
+    args <- getArgs
+    let dayn = read (args !! 0) :: Int
+    let sols = solutions !! (dayn - 1)
+    let fnam = args !! 1
+    putStrLn ("Solutions for day " ++ show dayn)
+    file <- readFile fnam
+    let (solA, solB) = sols
+    putStrLn . ("Problem A: " ++) . solA $ file
+    putStrLn . ("Problem B: " ++) . solB $ file
